d7e1ef4682d3435e0bb8b86a55bfc70e5dd1d82e
Anshumaan Reddy <anre4642@colorado.edu>
2025-02-25 14:01:53 -0700

    Set up XBee Coordinator for RPi 3B Client (https://github.com/anshubreddy/final-project-anshubreddy-client/issues/2)

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '663473cb0870c0b2cae6b3e01b9d3a28a5c81462'
+AESD_ASSIGNMENTS_VERSION = '74803ab918904f5c66010f202c87c474664cbf0a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,11 +16,13 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/bme280_sensor all
+        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/zigbee/zigbee_send all
 endef
 
 # TODO add the BME280 sensor utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/bme280_sensor/bme280 $(TARGET_DIR)/usr/bin
+        $(INSTALL) -m 0755 $(@D)/zigbee/zigbee_send/zigbee_send $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
--- base_external/post-image.sh
+++ base_external/post-image.sh
@@ -10,25 +10,34 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 for arg in "$@"
 do
-	case "${arg}" in
-		# I2C
-		--add-I2C)
-		if ! grep -qE '^dtparam=i2c_arm=on' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+ case "${arg}" in
+     # I2C
+     --add-I2C)
+     if ! grep -qE '^dtparam=i2c_arm=on' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+         cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
 # enable I2C
 dtparam=i2c_arm=on
 __EOF__
-		fi
-		;;
+     fi
+     ;;
 
-		--gpu_mem_256=*|--gpu_mem_512=*|--gpu_mem_1024=*)
-		# Set GPU memory
-		gpu_mem="${arg:2}"
-		sed -e "/^${gpu_mem%=*}=/s,=.*,=${gpu_mem##*=}," -i "${BINARIES_DIR}/rpi-firmware/config.txt"
-		;;
-	esac
+     --gpu_mem_256=*|--gpu_mem_512=*|--gpu_mem_1024=*)
+     # Set GPU memory
+     gpu_mem="${arg:2}"
+     sed -e "/^${gpu_mem%=*}=/s,=.*,=${gpu_mem##*=}," -i "${BINARIES_DIR}/rpi-firmware/config.txt"
+     ;;
 
+     # Serial Port
+     --add-serial)
+     echo "Copying init script for serial port configuration"
+     mkdir -p "${TARGET_DIR}/etc/init.d"
+     cp "${BOARD_DIR}/../../../base_external/rootfs_overlay/etc/init.d/S99serial_setup" "${TARGET_DIR}/etc/init.d/"
+     ;;
+ esac
 done
+
+# Ensure the script is executable
+chmod +x "${TARGET_DIR}/etc/init.d/S99serial_setup"
 
 # Pass an empty rootpath. genimage makes a full copy of the given rootpath to
 # ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
@@ -41,10 +50,10 @@ ROOTPATH_TMP="$(mktemp -d)"
 rm -rf "${GENIMAGE_TMP}"
 
 genimage \
-	--rootpath "${ROOTPATH_TMP}"   \
-	--tmppath "${GENIMAGE_TMP}"    \
-	--inputpath "${BINARIES_DIR}"  \
-	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
+ --rootpath "${ROOTPATH_TMP}"   \
+ --tmppath "${GENIMAGE_TMP}"    \
+ --inputpath "${BINARIES_DIR}"  \
+ --outputpath "${BINARIES_DIR}" \
+ --config "${GENIMAGE_CFG}"
 
 exit $?
